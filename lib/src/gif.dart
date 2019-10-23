6542ab1bc2533f933798fb14f4e7c6cf94f8fca8
dennougorilla <dennougorilla@gmail.com>
2019-10-23 15:34:35 +0900

    fix use_function_type_syntax_for_parameter

--- lib/src/gif.dart
+++ lib/src/gif.dart
@@ -15,26 +15,24 @@ class GIF {
 
 @JS()
 class Promise<T> {
-  external Promise(void executor(void resolve(T result), Function reject));
+  external Promise(void Function(Function(T), Function) executor);
   external Promise then(void Function(T) onFulfilled, [Function onRejected]);
 }
 
 @JS()
 @anonymous
 class Options {
+  external factory Options({int workers, int quality, int width, int height});
   external int get workers;
   external int get quality;
   external int get width;
   external int get height;
-
-  external factory Options({int workers, int quality, int width, int height});
 }
 
 @JS()
 @anonymous
 class AddFrameOptions {
+  external factory AddFrameOptions({int delay, bool copy});
   external int get delay;
   external bool get copy;
-
-  external factory AddFrameOptions({int delay, bool copy});
 }
